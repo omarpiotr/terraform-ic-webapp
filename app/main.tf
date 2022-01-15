6e78bd4660ff1f787d82811d9cfc5c2ddab80880
omarpiotr <omarpiotr.developer@gmail.com>
2022-01-15 20:56:03 +0100

    updates

--- app/main.tf
+++ app/main.tf
@@ -16,6 +16,7 @@ module "deploy_ec2_odoo"{
     serveur = "odoo"
     key_name = "capge_projet_kp"
     private_key_path = "D:/Formation/AJC/05.DevOps/PROJET/capge_projet_kp.pem"
+    sg_group = [ module.deploy_sg.sg_name]
 }
 
 module "deploy_ec2_master" {
@@ -27,6 +28,7 @@ module "deploy_ec2_master" {
     serveur = "AnsibleMaster"
     key_name = "capge_projet_kp"
     private_key_path = "D:/Formation/AJC/05.DevOps/PROJET/capge_projet_kp.pem"
+    sg_group = [ module.deploy_sg.sg_name]
     ec2_server_ip = module.deploy_ec2_server.ec2_ip
     ec2_odoo_ip = module.deploy_ec2_odoo.ec2_ip
 }
